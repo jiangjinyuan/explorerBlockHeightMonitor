19a276cbc01d9abf3b5214765eb58359627b5aad
jiangjinyuan <jinyuan.jiang@bitmain.com>
2019-10-29 15:41:42 +0800

    update cmd/general.go

--- cmd/general.go
+++ cmd/general.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/jiangjinyuan/explorerBlockHeightMonitor/models/height"
 	"github.com/jiangjinyuan/explorerBlockHeightMonitor/util"
+	"github.com/jiangjinyuan/explorerBlockHeightMonitor/configs"
 	"github.com/spf13/cobra"
 	"time"
 )
@@ -23,19 +24,19 @@ func GeneralCoinHeightMonitor(cmd *cobra.Command, args []string) {
 	switch coin {
 	case "btc":
 		util.ConfigLocalFilesystemLogger("./logs/", "btcMonitor.log", 7*time.Hour*24, time.Second*20)
-		Time=10 * time.Second
+		Time= time.Duration(configs.Config.TraverseSleepTime.Btc) * time.Second
 	case "bch":
 		util.ConfigLocalFilesystemLogger("./logs/", "bchMonitor.log", 7*time.Hour*24, time.Second*20)
-		Time=10 * time.Second
+		Time= time.Duration(configs.Config.TraverseSleepTime.Bch) * time.Second
 	case "ltc":
 		util.ConfigLocalFilesystemLogger("./logs/", "ltcMonitor.log", 7*time.Hour*24, time.Second*20)
-		Time=10 * time.Second
+		Time= time.Duration(configs.Config.TraverseSleepTime.Ltc) * time.Second
 	case "eth":
 		util.ConfigLocalFilesystemLogger("./logs/", "ethMonitor.log", 7*time.Hour*24, time.Second*20)
-		Time=5 * time.Second
+		Time= time.Duration(configs.Config.TraverseSleepTime.Eth) * time.Second
 	case "etc":
 		util.ConfigLocalFilesystemLogger("./logs/", "etcMonitor.log", 7*time.Hour*24, time.Second*20)
-		Time=5 * time.Second
+		Time= time.Duration(configs.Config.TraverseSleepTime.Etc) * time.Second
 	}
 	monitor := height.BlockHeightMonitor{}
 	db := util.InitDB()
